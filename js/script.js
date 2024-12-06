2e9e8d6b7b99ec57a6b4f7aaab0c8b50f6ba6c34
Artem Abramov <artemabramov230999@gmail.com>
2024-12-06 18:29:29 +0100

    add section hero and features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
-	
+  // hero-swiper 
+	const swiperHero = new Swiper('.hero-swiper', {
+    speed: 1000,
+    spaceBetween: 100,
+    autoplay: true,
+    dots: true,
+    navigation: {
+      nextEl: '.hero-swiper .swiper-button-next',
+      prevEl: '.hero-swiper .swiper-button-prev',
+    },
+    pagination: {
+      el: '.hero-swiper .swiper-pagination',
+      clickable: true,
+    },
+  });
 
 }, false);
